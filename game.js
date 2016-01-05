74122a4023840c630aa061a173b4efdfc1ba2167
Ben Rothman <croquetpro@gmail.com>
2016-01-05 11:44:40 -0800

    gravity and forces as well as the shape of the lander

--- game.js
+++ game.js
@@ -4,15 +4,13 @@ var context = canvas.getContext("2d");
 var spaceship =
 {
   color: "black",
-  width: 8,
-  height: 22,
-  position:
-  {
-    x: 100,
-    y: 100
-  },
+  width: 17,
+  height: 44,
+  position: { x: 0, y: 0 },
   angle: 0,
-  engineOn: true,
+  velocity: { x: 1, y: 0 },
+  thrust: 0.3,
+  engineOn: false,
   rotatingLeft: false,
   rotatingRight: false
 }
@@ -23,7 +21,24 @@ function drawSpaceship()
   context.beginPath();
   context.translate(spaceship.position.x, spaceship.position.y);
   context.rotate(spaceship.angle);
-  context.rect(spaceship.width * -0.5, spaceship.height * -0.5, spaceship.width, spaceship.height);
+  // context.rect(spaceship.width * -0.5, spaceship.height * -0.5, spaceship.width, spaceship.height);
+  context.arc(spaceship.width * 0.5, 2.5, 2.5, 0, 2*Math.PI)
+  context.moveTo(10, 1.5);
+  context.lineTo(12, 3);
+  context.lineTo(13, 6);
+  context.lineTo(16, 15);
+  context.lineTo(spaceship.width, 22);
+  context.lineTo(spaceship.width, spaceship.height - 3);
+  context.lineTo(spaceship.width - 3, spaceship.height - 3);
+  context.lineTo(spaceship.width, spaceship.height);
+  context.lineTo(0, spaceship.height);
+  context.lineTo(3, spaceship.height - 3);
+  context.lineTo(0, spaceship.height - 3);
+  context.lineTo(0, 22);
+  context.lineTo(1, 15);
+  context.lineTo(4, 6);
+  context.lineTo(5, 3);
+  context.lineTo(7, 1.5);
   context.fillStyle = spaceship.color;
   context.fill();
   context.closePath();
@@ -31,10 +46,10 @@ function drawSpaceship()
   if(spaceship.engineOn)
   {
     context.beginPath();
-    context.moveTo(spaceship.width * -0.5, spaceship.height * 0.5);
-    context.lineTo(spaceship.width * 0.5, spaceship.height * 0.5);
-    context.lineTo(0, spaceship.height * 0.5 + Math.random() * 5);
-    context.lineTo(spaceship.width * -0.5, spaceship.height * 0.5);
+    context.moveTo(0, spaceship.height);
+    context.lineTo(spaceship.width, spaceship.height);
+    context.lineTo(spaceship.width * 0.5, spaceship.height + 5 + Math.random() * 10);
+    context.lineTo(0, spaceship.height);
     context.closePath();
     context.fillStyle = "orange";
     context.fill();
@@ -42,17 +57,26 @@ function drawSpaceship()
   context.restore();
 }
 
+var gravity = 0.1;
+
 function updateSpaceship()
 {
+  spaceship.position.x += spaceship.velocity.x;
+  spaceship.position.y -= spaceship.velocity.y;
+
   if(spaceship.rotatingRight) {
     spaceship.angle += Math.PI / 180;
   } else if(spaceship.rotatingLeft) {
     spaceship.angle -= Math.PI / 180;
   }
   if(spaceship.engineOn) {
-    spaceship.position.x += Math.sin(spaceship.angle);
-    spaceship.position.y -= Math.cos(spaceship.angle);
+    // spaceship.position.x += Math.sin(spaceship.angle);
+    // spaceship.position.y -= Math.cos(spaceship.angle);
+    spaceship.velocity.x += spaceship.thrust * Math.sin(spaceship.angle);
+    spaceship.velocity.y += spaceship.thrust * Math.cos(spaceship.angle);
+
   }
+  spaceship.velocity.y -= gravity;
 }
 
 function draw() {
